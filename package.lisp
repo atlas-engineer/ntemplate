330a6b505be83ab9087394a302df2ea2e112dd06
Artyom Bologov <aartaka@protonmail.com>
2022-10-31 13:54:26 +0400

    package: Use proper package names syntax.

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 ;;;; SPDX-FileCopyrightText: (#| TMPL_VAR author |#)
 ;;;; SPDX-License-Identifier: (#| TMPL_VAR license |#)
 
-(uiop:define-package #:(#| TMPL_VAR name |#)
-  (:use #:common-lisp)
+(uiop:define-package :(#| TMPL_VAR name |#)
+  (:use :common-lisp)
   (:documentation "Describe `(#| TMPL_VAR name |#)' package here"))
